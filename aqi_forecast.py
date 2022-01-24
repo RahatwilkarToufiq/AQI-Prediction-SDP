fb23f590e61c9af4e4dda176353830e107bcc635
RahatwilkarToufiq <rhatwilkartoufiq@gmail.com>
2022-01-24 23:06:31 +0530

    upd

--- aqi_forecast.py
+++ aqi_forecast.py
@@ -4,12 +4,6 @@ import requests
 import pandas as pd
 from datetime import date, timedelta
 
-ts = int("1284101485")
-
-# if you encounter a "year is out of range" error the timestamp
-# may be in milliseconds, try `ts /= 1000` in that case
-print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
-
 
 def lat_lng(city):
 
